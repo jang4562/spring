69552d6f1441a410a32acb0205b26a8030d6fc92
Unknown <hss0736@naver.com>
2019-01-23 12:08:17 +0900

    로그인 기능 구현 - 유저정보 가져오기

--- springtest/src/main/java/kr/green/springtest/HomeController.java
+++ springtest/src/main/java/kr/green/springtest/HomeController.java
@@ -1,15 +1,13 @@
 package kr.green.springtest;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import kr.green.springtest.service.AccountService;
+import kr.green.springtest.vo.AccountVo;
 
 /**
  * Handles requests for the application home page.
@@ -17,14 +15,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
+	@Autowired
+	AccountService accountService;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String homeGet() {
 		return "home";
 	}
 	@RequestMapping(value = "/", method = RequestMethod.POST)
-	public String homePost(Model model, String id, String pw) {
-		System.out.println("id : " + id + ", pw : " + pw);
+	public String homePost(Model model,AccountVo loginInfo) {
+		AccountVo user = accountService.signin(loginInfo);
+		model.addAttribute("user",user);
 		return "redirect:/";
 	}
 	
--- springtest/src/main/java/kr/green/springtest/service/AccountServiceImp.java
+++ springtest/src/main/java/kr/green/springtest/service/AccountServiceImp.java
@@ -1,8 +1,26 @@
 package kr.green.springtest.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import kr.green.springtest.dao.AccountDao;
+import kr.green.springtest.vo.AccountVo;
 
 @Service
 public class AccountServiceImp implements AccountService {
+
+	@Autowired
+	AccountDao accountDao;
+	@Autowired
+	BCryptPasswordEncoder passwordEncoder;
+	
+	@Override
+	public AccountVo signin(AccountVo loginInfo) {
+		AccountVo user = accountDao.getAccount(loginInfo.getId());
+		if(passwordEncoder.matches(loginInfo.getPw(),user.getPw()))
+			return user;
+		return null; 
+	}
 
 }
