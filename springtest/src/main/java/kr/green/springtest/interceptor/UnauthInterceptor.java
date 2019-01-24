505d40ddd96038fb15a32af45e6b39187382827c
Unknown <hss0736@naver.com>
2019-01-24 10:21:11 +0900

    회원,비회원 접근 관리 기능 추가(Interceptor)

---
+++ springtest/src/main/java/kr/green/springtest/interceptor/UnauthInterceptor.java
@@ -0,0 +1,24 @@
+package kr.green.springtest.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class UnauthInterceptor extends HandlerInterceptorAdapter {
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, 
+			HttpServletResponse response, 
+			Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		Object user = session.getAttribute("user");
+//		AccountVo user = (AccountVo)session.getAttribute("user"); // 이렇게 써도 상관은없음
+		if(user != null) {
+			response.sendRedirect(request.getContextPath()+"/bbs/list");
+		}
+		return true;
+	}
+}
